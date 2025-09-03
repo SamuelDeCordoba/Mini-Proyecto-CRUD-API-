bb87978168708786ed0de9f3f7429eb9e3fe9863
Sammy <cordoba.samuel.5631@eam.edu.co>
2025-09-03 15:28:19 -0500

    routers(api): added logic routers

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\VentaController;
+use App\Http\Controllers\ReporteController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// CRUD completo de productos
+Route::apiResource('productos', ProductoController::class);
+
+// Rutas de ventas
+Route::get('ventas', [VentaController::class, 'index']);
+Route::post('ventas', [VentaController::class, 'store']);
+
+// Reporte de ventas
+Route::get('reporte-ventas', [ReporteController::class, 'reporteVentas']);
