172c21b00180c9e9e1bc4ea22553d0e5475fc44c
Sammy <cordoba.samuel.5631@eam.edu.co>
2025-09-03 15:10:26 -0500

    database(venta): added attributes

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Venta extends Model
 {
     use HasFactory;
+
+    protected $table = 'ventas';
+
+    protected $fillable = [
+        'producto_id',
+        'cantidad',
+        'fecha'
+    ];
+
+    protected $casts = [
+        'fecha' => 'datetime'
+    ];
+
+    // Relación con producto
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
 }
